bc7140db0cc42a1f4dbccaa2fcea94c5bcae4e0d
jisoo <93499095+amisu1203@users.noreply.github.com>
2021-12-11 23:59:53 +0900

    Update index.js

--- modal/index.js
+++ modal/index.js
@@ -2,4 +2,6 @@ const btnOpen = document.getElementById('btn-open');
 const btnClose = document.getElementById('btn-close');
 const ContModal = document.querySelector('.cont-modal');
 
-btn-open.onclick
+btn-open.onclick = () {
+  cont.modal.
+}
