51d057258922c889df33a1baad22919bc42a6e73
jisoo <93499095+amisu1203@users.noreply.github.com>
2021-12-14 23:39:07 +0900

    hp 0일때 bunker 사라짐 수정

--- JavaScript/미니스타그래프트/index.js
+++ JavaScript/미니스타그래프트/index.js
@@ -1,12 +1,19 @@
 // drone 클릭하면 spit됨
 
+let hp = 3;
 $('#drone').click(function () {
     console.log('드론이 클릭됐습니다.');
     $('#spit').fadeIn(200);
     $('#spit').animate({
         left: '+=250'
     });
-    $('#spit').fadeOut();
+    $('#spit').fadeOut(function () {
+        hp -= 1;
+        $('#hp').text(`HP : ${hp}`);
+        if (hp === 0) {
+            $('#bunker').fadeOut(300, 'linear');
+        }
+    });
     $('#spit').animate({
         // 원래 위치로 옮김
         left: '150px'
